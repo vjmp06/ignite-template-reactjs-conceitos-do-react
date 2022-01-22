57d092d1f547124804111512eb45f82781dad128
Vinicius Moreira <vinicius_moreira06@hotmail.com>
2022-01-22 12:11:54 -0300

    desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return "Titulo não pode ser vazio!";
+    }
+    const task: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, task]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task: Task = tasks.find(task => task.id == id);
+    
+    handleRemoveTask(id);
+    task.isComplete = true;
+    setTasks([...tasks, task]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id != id));
   }
 
   return (
